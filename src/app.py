95c3a2f5152ef1d8cdd6e55a626b8ab39f564c2c
Akash0913 <44508860+Akash0913@users.noreply.github.com>
2024-12-13 03:18:13 +0000

    First commit

--- src/app.py
+++ src/app.py
@@ -1,4 +1,74 @@
-from utils import db_connect
-engine = db_connect()
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.feature_selection import SelectKBest, f_regression
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.linear_model import Lasso
 
-# your code here
+total_data = pd.read_csv("https://raw.githubusercontent.com/4GeeksAcademy/regularized-linear-regression-project-tutorial/main/demographic_health_data.csv", sep = ",")
+print(total_data.head())
+total_data = total_data.drop_duplicates().reset_index(drop = True)
+print(total_data.head())
+data_types = total_data.dtypes
+numeric_columns = [c for c in list(data_types[data_types != "object"].index) if c != "Heart disease_number"]
+
+scaler = StandardScaler()
+norm_features = scaler.fit_transform(total_data[numeric_columns])
+
+total_data_scal = pd.DataFrame(norm_features, index = total_data.index, columns = numeric_columns)
+total_data_scal["Heart disease_number"] = total_data["Heart disease_number"]
+print(total_data_scal.head())
+
+X = total_data_scal.drop(columns=["Heart disease_number"])
+y = total_data_scal["Heart disease_number"]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
+train_indices = list(X_train.index)
+test_indices = list(X_test.index)
+
+k = int(len(X_train.columns) * 0.3)
+selection_model = SelectKBest(score_func = f_regression, k = k)
+selection_model.fit(X_train, y_train)
+ix = selection_model.get_support()
+
+X_train_sel = pd.DataFrame(selection_model.transform(X_train), columns = X_train.columns.values[ix])
+X_test_sel = pd.DataFrame(selection_model.transform(X_test), columns = X_test.columns.values[ix])
+
+print(X_train_sel.head())
+print(X_test_sel.head())
+
+X_train_sel["Heart disease_number"] = list(y_train)
+X_test_sel["Heart disease_number"] = list(y_test)
+
+X_train_sel.to_csv("clean_train.csv", index = False)
+X_test_sel.to_csv("clean_test.csv", index = False)
+
+total_data = pd.concat([X_train_sel, X_test_sel])
+print(total_data.head())
+print(X_test_sel.head())
+
+train_data = pd.read_csv("clean_train.csv")
+test_data = pd.read_csv("clean_test.csv")
+
+print(train_data.head())
+
+X_train = train_data.drop(["Heart disease_number"], axis = 1)
+y_train = train_data["Heart disease_number"]
+X_test = test_data.drop(["Heart disease_number"], axis = 1)
+y_test = test_data["Heart disease_number"]
+
+model = LogisticRegression()
+print(model.fit(X_train, y_train))
+print(f"Intercep (a): {model.intercept_}")
+print(f"Coefficients: {model.coef_}")
+y_pred = model.predict(X_test)
+print(y_pred)
+print(f"MSE: {mean_squared_error(y_test, y_pred)}")
+print(f"R2 Score: {r2_score(y_test, y_pred)}")
+
+alpha = 1.0
+lasso_model = Lasso(alpha = alpha)
+lasso_model.fit(X_train, y_train)
+score = lasso_model.score(X_test, y_test)
+print("Coefficients:", lasso_model.coef_)
+print("R2 score:", score)
